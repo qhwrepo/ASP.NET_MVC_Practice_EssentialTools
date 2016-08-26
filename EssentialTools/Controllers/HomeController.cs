e148f00ada45c8e0eefa12d3c9e2c4acaa7b7dda
Qinghang Wu <Qinghang Wu>
2016-08-26 15:26:32 +1000

    Using Ninject

--- EssentialTools/Controllers/HomeController.cs
+++ EssentialTools/Controllers/HomeController.cs
@@ -4,25 +4,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Ninject;
 
 namespace EssentialTools.Controllers
 {
     public class HomeController : Controller
     {
+        private IValueCalculator calc;
         private Product[] products = {
             new Product {Name = "Kayak", Category = "Watersports" ,Price = 275M },
             new Product {Name = "Lifejacket", Category = "Watersports", Price = 48.95M },
             new Product {Name = "Soccer ball", Category = "Soccer", Price = 19.50M },
             new Product {Name = "Corner flag", Category = "Soccer", Price = 34.95M }
         };
+
+        public HomeController(IValueCalculator calcParam)
+        {
+            calc = calcParam;
+        }
         // GET: Home
         public ActionResult Index()
         {
-            //LinqValueCalculator calc = new LinqValueCalculator();
-            IValueCalculator calc = new LinqValueCalculator();
+            
+            //IValueCalculator calc = new LinqValueCalculator();
             ShoppingCart cart = new ShoppingCart(calc);
             cart.Products = products;
-            decimal totalValue = cart.CalculateProductTotal();
+
+            decimal? totalValue = cart.CalculateProductTotal();
             return View(totalValue);
         }
     }
